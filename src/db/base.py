681fe16f2b9012f03557ff6dd39ea2f0b3199eb1
Mark Roman <mroman8465@gmail.com>
2025-11-14 20:28:47 -0500

    Now looking for DATABASE_URL from environment before trying to connect to PostgreSQL so the GitHub Actions work.

--- src/db/base.py
+++ src/db/base.py
@@ -6,23 +6,29 @@ import os
 
 # Load variables from .env into environment
 load_dotenv()
-DATABASE_URL = (
-    f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}"
-    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-)
+
+# Prefer DATABASE_URL if set (e.g. in CI)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    # Fall back to building Postgres URL from individual parts
+    DATABASE_URL = (
+        f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}"
+        f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+    )
 
 # Create a connection to the PostgreSQL database. engine is an
 # instance of the SQLAlchemy engine. SQLAlchemy will use this
 # to send queries and receive results. echo = True will print every SQL
 # statement to the console.
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo = True)
 
 # Create session factory so sessions can talk to the database.
 # SessionLocal is a function call that behaves like a class and
 # returns new Session instances. SessionLocal() will start a session
 # when we want to add new data, query existing data, or commit changes
 # to the database.
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(bind = engine)
 
 # Base class for all models. All tables will inherit from this. It
 # registers models so SQLAlchemy knows how to map them to actual
