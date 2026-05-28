48e380fcb762bf25cc1f3f98a7cf9d0fd1ae3e69
Mark Roman <mroman8465@gmail.com>
2026-05-28 15:22:43 -0400

    Added scripts for loading the right .env file and ingesting and seeding tea profiles for staging. Split backend.yml into two files, ci_backend.yml for all backend continuous integration and cd_staging.yml for deploying to staging using the deploy branch.

--- scripts/PowerShell/ingest_tea_profiles_staging.ps1
+++ scripts/PowerShell/ingest_tea_profiles_staging.ps1
@@ -1,11 +1,14 @@
-# .\scripts\PowerShell\ingest_tea_profiles.ps1
-Write-Host "Ingesting data for tea_profiles database..."
+# .\scripts\PowerShell\ingest_tea_profiles_staging.ps1
+Write-Host "Ingesting tea_profiles data in STAGING..."
 
 # Ensure we're running from repo root so Python can resolve src.*
 Set-Location "$PSScriptRoot\..\.."
 
+# Load staging environment variables
+& "$PSScriptRoot\load_env.ps1" -EnvFilePath "$PSScriptRoot\..\..\.env.staging"
+
 # Activate venv if needed
-& "$PSScriptRoot\..\..\venv\Scripts\Activate.ps1"
+& ".\venv\Scripts\Activate.ps1"
 
 # Run the Python ingestion module
 python -m src.app.ingest_tea_profiles
@@ -16,4 +19,4 @@ if ($LASTEXITCODE -ne 0) {
     exit $LASTEXITCODE
 }
 
-Write-Host "Ingestion complete"
+Write-Host "Ingestion complete in STAGING"
